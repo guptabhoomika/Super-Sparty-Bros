398170eff4b52b635acce9fcc5f400ec218a7bcb
guptabhoomika <bhoomika.189301123@muj.manipal.edu>
2019-07-09 13:55:02 +0530

    Added Super sparty Bros full project

---
+++ New Unity Project (4)/Assets/Scripts/MenuButtonLoadLevel.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement; // include so we can load new scenes
+
+public class MenuButtonLoadLevel : MonoBehaviour {
+
+	public void loadLevel(string levelToLoad)
+	{
+		SceneManager.LoadScene(levelToLoad);
+	}
+    public void retry(string level)
+    {
+        SceneManager.LoadScene(level);
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+    public void About(string about)
+    {
+        SceneManager.LoadScene(about);
+    }
+ 
+}
---
+++ New Unity Project (4)/Assets/Scripts/about.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class about : MonoBehaviour
+{
+   public void Loadmenu(string menu)
+    {
+        SceneManager.LoadScene(menu);
+    }
+}
